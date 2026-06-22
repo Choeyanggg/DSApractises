8d93c9ff722fa9d3a1559de9fa36f72ad22a1cff
Tenzin Choeyang <137075782+Choeyanggg@users.noreply.github.com>
2026-06-22 11:28:39 +0530

    Time: 47 ms (65.18%) | Memory: 29.2 MB (30.07%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
         sort(nums.begin(),nums.end());
-        vector<vector<int>> final;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.size();i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-            int fix=nums[i];
-            int start=i+1;
-            int end=n-1;
-            while(start<end){
-                int res=fix+nums[start]+nums[end];
-                if(res==0){
-                    final.push_back({fix,nums[start],nums[end]});
-                    start++;
-                    end--;
-                    while(start<end && nums[start]==nums[start-1]){
-                        start++;
-                    }
-                }else if(res<0){
-                    start++;
+            int j=i+1;
+            int k=nums.size()-1;
+            while(j<k){
+                int result=nums[i]+nums[j]+nums[k];
+                if(result<0){
+                    j++;
+                }else if(result>0){
+                    k--;
                 }else{
-                    end--;
+                    res.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
                 }
             }
         }
-        return final;
+        return res;
     }
 };
