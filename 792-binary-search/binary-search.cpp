79736ced2577fe091ec428b134ef5572785f5700
Tenzin Choeyang <137075782+Choeyanggg@users.noreply.github.com>
2026-06-30 10:52:43 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (81.31%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int right=nums.size();
+        int left=0;
+        int mid=0;
+        while(left<right){
+            mid=left+(right-left)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) left=mid+1;
+            else right=mid;
+        }
+        return -1;
+    }
+};
