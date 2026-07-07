90dfe28a22db0b40600ffe682229f066c2461e8a
Tenzin Choeyang <137075782+Choeyanggg@users.noreply.github.com>
2026-07-07 11:52:40 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (19.46%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> res {-1,-1};
-        int left=0, right=nums.size()-1;
-        while(left<=right){
+        vector<int>res(2,-1);
+        int left=0, right=nums.size();
+        while(left<right){
             int mid=left+(right-left)/2;
             if(nums[mid]==target){
                 res[0]=mid;
-                right=mid-1;
+                right=mid;
             }else if(nums[mid]>target){
-                right=mid-1;
-            }else left=mid+1;
+                right=mid;
+            }else{
+                left=mid+1;
+            }
         }
-        if(res[0]==-1) return res;
-
-        left=0, right=nums.size()-1;
-        while(left<=right){
+        left=0, right=nums.size();
+        while(left<right){
             int mid=left+(right-left)/2;
             if(nums[mid]==target){
                 res[1]=mid;
                 left=mid+1;
-            }else if(nums[mid]>target){
-                right=mid-1;
-            }else left=mid+1;
+            }else if(nums[mid]<target){
+                left=mid+1;
+            }else{
+                right=mid;
+            }
         }
         return res;
     }
