528b39e904ae709e65810ed0c31262e956219845
Tenzin Choeyang <137075782+Choeyanggg@users.noreply.github.com>
2026-07-08 10:57:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (82.06%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<=0) return false;
-        while(n%2==0){
-            n/=2;
-        }
-        return n==1;
+        else if(n==1) return true;
+        else if(n%2!=0) return false;
+        return isPowerOfTwo(n/2);
     }
 };
