552d0a21a9f34a2efe90d5a88c2996832d623bdd
Tenzin Choeyang <137075782+Choeyanggg@users.noreply.github.com>
2026-07-26 16:30:41 +0530

    Time: 74 ms (29.21%) | Memory: 113.5 MB (5.32%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int n:nums){
+            mp[n]++;
+        }
+        for(auto m:mp){
+            if(m.second>1) return true;
+        }return false;
+    }
+};
