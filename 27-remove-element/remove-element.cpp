3e2e9008bb87b41127b6cde2b09daeb6a94b6051
Tenzin Choeyang <137075782+Choeyanggg@users.noreply.github.com>
2026-07-26 17:20:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (85.25%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -4,7 +4,8 @@ public:
         int j=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=val){
-                swap(nums[i],nums[j++]);
+                swap(nums[i],nums[j]); 
+                j++;
             }
         }return j;
     }
