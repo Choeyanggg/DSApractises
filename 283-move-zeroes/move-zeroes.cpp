12ea5f910dd34015182541e5a4ee0d955307445c
Tenzin Choeyang <137075782+Choeyanggg@users.noreply.github.com>
2026-08-17 23:58:14 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (56.57%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,7 +4,7 @@ public:
         int j=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
-                swap(nums[j++],nums[i]);
+                swap(nums[i],nums[j++]);
             }
         }
     }
