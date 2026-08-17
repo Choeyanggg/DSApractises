cf986feb162f7d3aaeee9d71fc247e85ffcf47f9
Tenzin Choeyang <137075782+Choeyanggg@users.noreply.github.com>
2026-08-18 00:29:17 +0530

    Time: 7 ms (26.92%) | Memory: 18.3 MB (93.19%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    struct Compare{
+        bool operator()(ListNode* a, ListNode* b){
+            return a->val>b->val;
+        }
+    };
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*,vector<ListNode*>,Compare> pq;
+        for(auto x:lists){
+            if(x!=nullptr){
+                pq.push(x);
+            }
+        }
+        ListNode* dummy=new ListNode(0);
+        ListNode* tail=dummy;
+        while(!pq.empty()){
+            ListNode* x=pq.top();
+            pq.pop();
+            tail->next=x;
+            tail=tail->next;
+            if(x->next!=nullptr){
+                pq.push(x->next);
+            }
+        }
+        return dummy->next;
+    }
+};
