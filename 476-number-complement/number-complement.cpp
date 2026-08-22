37607361b2f287bd5b254c65c2831ecfc5ebe555
Tenzin Choeyang <137075782+Choeyanggg@users.noreply.github.com>
2026-08-23 01:43:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (97.13%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
     int findComplement(int num) {
-        int binary[32];
-        int i=0;
-        while(num>0){
-            binary[i++]=num%2;
-            num/=2;
+        int comp=0;
+        int temp=num;
+        while(temp>0){
+            comp=(comp<<1)| 1;
+            temp>>=1;
         }
-        int arr[i];
-        int k=0;
-        for(int j=i-1;j>=0;j--){
-            if(binary[j]){
-                arr[k++]=0;
-            }else{
-                arr[k++]=1;
-            }
-        }
-        int result=0;
-        for(int j=0;j<i;j++){
-            result<<=1;
-            result|=arr[j];
-        }
-        return result;
+        return num^comp;
     }
 };
