598b505cc1938f0a488c113d49ce02c6af72e72f
Tenzin Choeyang <137075782+Choeyanggg@users.noreply.github.com>
2026-09-06 20:07:50 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (26.44%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.cpp
+++ 777-toeplitz-matrix/toeplitz-matrix.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        int row=matrix.size();
-        int col=matrix[0].size();
-        for(int i=0;i<row-1;i++){
-            for(int j=0;j<col-1;j++){
-                if(matrix[i][j]!=matrix[i+1][j+1]) return false;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        bool is=true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i!=0 && j!=0 && matrix[i][j]!=matrix[i-1][j-1]){
+                    is=false;
+                }
             }
-        }return true;
+        }return is;
     }
 };
