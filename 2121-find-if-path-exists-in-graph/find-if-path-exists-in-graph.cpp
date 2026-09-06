eae13a2bf27519a438f43ebc3d9ed60235c89e55
Tenzin Choeyang <137075782+Choeyanggg@users.noreply.github.com>
2026-09-06 20:41:26 +0530

    Time: 389 ms (38.79%) | Memory: 311.7 MB (41.96%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj, vector<bool> &visited,int source,int destination){
-        if(source==destination) return true;
-        visited[source]=true;
-        for(auto a:adj[source]){
-            if(!visited[a]){
-                if(dfs(adj,visited,a,destination)){
-                    return true;
-                }
-            }
-        }return false;
-    }
-
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         vector<vector<int>> adj(n);
         for(auto e:edges){
@@ -19,6 +7,19 @@ public:
             adj[e[1]].push_back(e[0]);
         }
         vector<bool>visited(n,false);
-        return dfs(adj,visited,source,destination);
+        queue<int> q;
+        q.push(source);
+        visited[source]=true;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            if(node==destination) return true;
+            for(int a:adj[node]){
+                if(!visited[a]){
+                    q.push(a);
+                    visited[a]=true;
+                }
+            }
+        }return false;
     }
 };
