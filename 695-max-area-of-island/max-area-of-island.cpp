ff7c63fe5b6ac5ccb28ed4072aa4ddf72346c170
Tenzin Choeyang <137075782+Choeyanggg@users.noreply.github.com>
2026-09-07 15:31:33 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (79.05%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
+    void island(int &area,vector<vector<int>>& grid,int r, int c){
+        if(r<0 || r>=grid.size() || c<0 || c>=grid[0].size() || grid[r][c]==0) return;
+        grid[r][c]=0;
+        area+=1;
+
+        island(area,grid,r-1,c);
+        island(area,grid,r+1,c);
+        island(area,grid,r,c-1);
+        island(area,grid,r,c+1);
+    }
+
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int maxi=INT_MIN;
-        vector<vector<int>> visited(n,vector<int>(m,0));
-        vector<vector<int>> directions={{-1,0},{1,0},{0,-1},{0,1}};
-        queue<pair<int,int>> q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && !visited[i][j]){
-                    int count=0;
-                    visited[i][j]=1;
-                    q.push({i,j});
-                    while(!q.empty()){
-                        auto[r,c]=q.front();
-                        q.pop();
-                        count++;
-                        for(auto dir:directions){
-                            int nr=r+dir[0];
-                            int nc=c+dir[1];
-                            if(nr>=0 && nr<n && nc>=0 && nc<m){
-                                if(grid[nr][nc]==1 && !visited[nr][nc]){
-                                    q.push({nr,nc});
-                                    visited[nr][nc]=1;
-                                }
-                            }
-                        }
-                    }maxi=max(maxi,count);
+        int maxarea=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    int area=0;
+                    island(area,grid,i,j);
+                    maxarea=max(maxarea,area);
                 }
             }
-        }return maxi==INT_MIN?0:maxi;
+        }return maxarea;
     }
 };
