974737bc0a14c26c1b6f1b73a4e6cf615217dd28
Tenzin Choeyang <137075782+Choeyanggg@users.noreply.github.com>
2026-09-08 01:08:03 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (97.49%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    bool check(vector<vector<char>>& board, int r,int c, vector<vector<bool>>& visited){
-        if(r<0 || r>=board.size() || c<0 || c>=board[0].size()) return false;
-        if(board[r][c]=='X') return true;
-        if(visited[r][c]) return true;
-        visited[r][c]=true;
-
-        bool up=check(board,r-1,c,visited);
-        bool down=check(board,r+1,c,visited);
-        bool left=check(board,r,c-1,visited);
-        bool right=check(board,r,c+1,visited);
-        return up && down && left && right;
-    }
-    void dfs(vector<vector<char>>& board,int r, int c){
+    void dfs(vector<vector<char>>& board,int r,int c){
         if(r<0 || r>=board.size() || c<0 || c>=board[0].size() || board[r][c]!='O') return;
-        board[r][c]='X';
+        board[r][c]='#';
 
         dfs(board,r-1,c);
         dfs(board,r+1,c);
         dfs(board,r,c-1);
         dfs(board,r,c+1);
     }
-
     void solve(vector<vector<char>>& board) {
         int n=board.size();
         int m=board[0].size();
         for(int i=0;i<n;i++){
+            if(board[i][0]=='O'){
+                dfs(board,i,0);
+            }if(board[i][m-1]=='O'){
+                dfs(board,i,m-1);
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(board[0][i]=='O'){
+                dfs(board,0,i);
+            }if(board[n-1][i]=='O'){
+                dfs(board,n-1,i);
+            }
+        }
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j]=='O'){
-                    vector<vector<bool>> visited(n, vector<bool>(m,false));
-                    if(check(board,i,j,visited)){
-                        dfs(board,i,j);
-                    }
+                if(board[i][j]=='#'){
+                    board[i][j]='O';
+                }else{
+                    board[i][j]='X';
                 }
             }
         }
